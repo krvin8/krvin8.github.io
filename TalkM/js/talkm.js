dd6009d32ff4bf9fdcc2b68c765226537ab03f8c
Kervin Tan <kervintanph@gmail.com>
2017-12-17 10:45:28 +0800

    Removed fittext js

--- TalkM/js/talkm.js
+++ TalkM/js/talkm.js
@@ -27,14 +27,6 @@
         $('.navbar-toggle:visible').click();
     });
 
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '28px',
-            maxFontSize: '42px'
-        }
-    );
-
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
